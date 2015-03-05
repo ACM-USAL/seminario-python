abf46e2ef8a22afb2126a28ec36e22c7470bf11a
Alternhuman <diegomartinpi@gmail.com>
2015-03-05 15:11:01 +0100

    Scripts de bucles

---
+++ codigo/Scripts-Informatica/bucles.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+lista = []
+cadena = input("Dame una cadena\n")
+while cadena != "":
+	lista.append(cadena)
+	cadena = input("Dame una cadena\n")
+else:
+	for elemento in lista:
+		print(elemento)
+
+lista = range(0,20)
+suma = 0
+for elemento in lista:
+	if elemento % 2 == 0:
+		continue
+	else:
+		suma += elemento
+
+print(suma)
+
+lista = []
+
+while True:
+	cadena = input("Dame una cadena\n")
+	if cadena == "":
+		break
+	lista.append(cadena)
+
+lista.sort()
+print(lista)
+
