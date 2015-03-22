d8543e61c0ae171662bccf5ac654faa7e7113935
Dani García <dani14_96@usal.es>
2015-03-23 00:14:51 +0100

    Added Concienciate's pres. Added bioscripts

---
+++ codigo/Scripts/ProtDB.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 22 20:41:55 2015
+
+@author: Dani
+"""
+import urllib2, re, webbrowser
+F = ''
+while not F:
+    standard_url_rcbs = 'http://www.PDB.org/pdb/explore.do?structureId='
+    add_url = raw_input('Insert valid RCSB protein code: ').upper()
+    if add_url == 'FIN':
+        print 'Bye bye'
+        exit()
+    type(add_url)
+    final_url =standard_url_rcbs + add_url
+    F = urllib2.urlopen(final_url)
+db = 10
+error_count = 0
+while db > 4 or db < 1 and error_count < 3:
+    db = int(raw_input('Choose a database number [PDB(1), UniProt(2), UniProt fasta(3)]: '))
+    if db == 1:
+        webbrowser.open(final_url)
+        break
+    elif db == 2:
+        webbrowser.open('http://www.rcsb.org/pdb/explore/jmol.do?structureId=' + add_url)
+        break
+    elif db == 3 or db == 4:    
+        for line in F:
+            url = re.search('href=\"(http://www\.uniprot\.org/uniprot/.+?)\"',F.read())
+            if url:
+                if db == 3:
+                    print url.group(1)
+                    webbrowser.open(url.group(1))
+                elif db == 4:
+                    print url.group(1) + '.fasta'
+                    webbrowser.open(url.group(1) + '.fasta')
+                break
+            else: 
+                print "Script isn't working. Sorry for the inconvenience"
+                exit()
+        break
+    else: error_count += 1
+else: 
+    if error_count == 3: 
+        print 'User not responding, exiting...'
+        exit()
+print 'Thank you for using.'
